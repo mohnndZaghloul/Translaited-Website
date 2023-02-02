691290c192e14b814a368dc6f1df03d50ffd6127
mohnndZaghloul <123903688+mohnndZaghloul@users.noreply.github.com>
2023-02-02 03:17:17 +0200

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,115 @@
+const translations = {
+    en:{
+        Home: "Home",
+        About: "About",
+        Services:"Services",
+        Projects:"Projects",
+        NewsUpdates:"News & Updates",
+        ConnectUs:"Connect Us",
+        contact:"CONTACT",
+        text1:"Al-Ukool Al-Thakiea",
+        text2:"Industrial Control and Automation Specialist",
+        text3:"Our Services",
+        Design:"Design",
+        text4:"Whatever your needs, we design the control system the meet your system requirements",
+        MoreInfo:"More Info",
+        Supply:"Supply",
+        text5:"Supply all Industrial Automation Equipments from various global leading manufacturers",
+        Commissioning:"Commissioning",
+        text6:"Handle Tests, Commissioning and Start-Up activities as well as  system maintenance.",
+        text7:"Depend on global automation leaders",
+        OurCustomers:"Our Customers",
+        text8:"Need more details? Contact us",
+        text9:"We are here to assist. Contact us by phone, email or via our social media channels.",
+        ContactUs:"Contact Us",
+        text10:"© 2023 by Al-Okool Al-Thakiea",
+        text11:"The Axis Group",
+        text12:"I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.",
+        text13:"This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.",
+        Ourteam:"Our team",
+        text14:"This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.",
+        text15:"Industrial Control and Automation Company",
+        text16:"Valid for one month",
+        text17:"Valid for three month",
+        text18:"Valid for six month",
+        text19:"No posts published in this language yet",
+        text20:"Once posts are published, you’ll see them here",
+        text21:"Axis Group",
+        fname:"First Name *",
+        lname:"Last Name *",
+        email:"Email *",
+        subject:"Subject *",
+        message:"Message *",
+        send:"Send",
+        text22:"Our Address",
+        text23:"Call Us",
+    },
+    ar:{
+        Home: "الرئيسية",
+        About: "عن الشركة",
+        Services:"خدماتنا",
+        Projects:"مشاريعنا",
+        NewsUpdates:"الاخبار و التحديثات",
+        ConnectUs:"اتصل بنا",
+        contact:"اتصل",
+        text1:"العقول الذكية",
+        text2:"متخصصون في السيطرة الصناعية والاتمتة",
+        text3:"خدماتنا",
+        Design:"التصميم",
+        text4:"تصميم منظومات السيطرة بما يغطي متطلبات المنظومة",
+        MoreInfo:"المزيد",
+        Supply:"التجهيز",
+        text5:"تجهيز معدات السيطرة بمختلف انواعها ومن افضل المناشئ العالمية",
+        Commissioning:"الفحص والتشغيل",
+        text6:"نقوم باعمال الفحص والتشغيل الاولي والنهائي بالاضافة الى اعمال الصيانة الدورية والطارئة",
+        text7:"نعتمد على افضل المصنعين لاجهزة السيطرة",
+        OurCustomers:"عملاؤنا",
+        text8:"هل تحتاج إلى مزيد من التفاصيل؟ اتصل بنا",
+        text9:"نحن هنا للمساعدة. تواصل معنا عبر الهاتف أو البريد الإلكتروني أو عبر قنوات التواصل الاجتماعي الخاصة بنا.",
+        ContactUs:"اتصل بنا",
+        text10:"جميع الحقوق محفوظة، العقول الذكية، ٢٠٢٣",
+        text11:"مجموعة المحور",
+        text12:"أنا فقرة. انقر هنا لإضافة النص الخاص بك وتحريرني. من السهل. فقط انقر فوق \"تحرير النص\" أو انقر نقرًا مزدوجًا فوقي لإضافة المحتوى الخاص بك وإجراء تغييرات على الخط. لا تتردد في السحب والإسقاط في أي مكان تريده على صفحتك. أنا مكان رائع بالنسبة لك لسرد قصة والسماح للمستخدمين بمعرفة المزيد عنك.",
+        text13:"هذه مساحة رائعة لكتابة نص طويل عن شركتك وخدماتك. يمكنك استخدام هذه المساحة للدخول في مزيد من التفاصيل حول شركتك. تحدث عن فريقك والخدمات التي تقدمها. أخبر زوارك قصة كيف توصلت إلى فكرة لعملك وما الذي يجعلك مختلفًا عن منافسيك. اجعل شركتك مميزة وأظهر للزائرين من أنت. ​",
+        Ourteam:"فريقنا",
+        text14:"هذه مساحة رائعة لكتابة نص طويل عن شركتك وخدماتك. يمكنك استخدام هذه المساحة للدخول في مزيد من التفاصيل حول شركتك. تحدث عن فريقك والخدمات التي تقدمها. أخبر زوارك قصة كيف توصلت إلى فكرة لعملك وما الذي يجعلك مختلفًا عن منافسيك. اجعل شركتك مميزة وأظهر للزائرين من أنت.",
+        text15:"السيطرة الصناعية والاتمتة",
+        text16:"سارٍ حتى شهر واحد",
+        text17:"سارٍ حتى 3 أشهر",
+        text18:"سارٍ حتى 6 أشهر",
+        text19:"لم يتم نشر أي منشورات بهذه اللغة حتى الآن",
+        text20:"...انتظرونا",
+        text21:"مجموعة المحور",
+        fname:"الاسم الاول *",
+        lname:"الكنية *",
+        email:"بريد إلكتروني *",
+        subject:"موضوع *",
+        message:"رسالة *",
+        send:"ارسال",
+        text22:"عنواننا",
+        text23:"اتصل بنا",
+    }
+}
+//seting lang in loacal storage
+var lang = localStorage.getItem('lang');
+const en = document.getElementById('en');
+const ar = document.getElementById('ar');
+en.addEventListener('click',()=>{
+    localStorage.setItem("lang","en");
+    location.reload();
+})
+ar.addEventListener('click',()=>{
+    localStorage.setItem("lang","ar");
+    location.reload();
+})
+//changing text in page 
+const elements = document.querySelectorAll("[data-tran]");
+elements.forEach((element)=>{
+    const translationsKey = element.getAttribute('data-tran');
+    element.textContent = translations[lang][translationsKey];
+    if(lang == 'en'){
+        document.body.style.direction ="ltf";
+    }else{
+        document.body.style.direction ="rtl";        
+    }
+})
